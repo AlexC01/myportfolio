9d3d5c7e6505e3da3fb9e3350dab9edf3c5e18fe
AlexC01 <acuadraq@gmail.com>
2021-12-19 14:04:52 -0600

    ✨Creating the index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const index = () => {
+  return (
+    <Layout>
+      <div>
+        <h1 className="font-bold text-2xl text-red-400">Testing</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default index

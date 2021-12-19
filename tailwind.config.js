2575d997277e691db5cb094feec75343158143e7
AlexC01 <acuadraq@gmail.com>
2021-12-19 14:04:21 -0600

    🔧 Adding colors and fonts to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        darkcolor: "#1B262C",
+        textnav: "#E8F0F2",
+        babyblue: "#BBE1FA",
+        lightblue: "#3282B8",
+        hoverblue: "#B3D5EB",
+        thisblue: "#0F4C75",
+        hoverbutton: "#DCE7EA",
+        hoverdark: "#2B3B44",
+        hoverlastblue: "#1D5E89",
+      },
+    },
   },
   plugins: [],
 }
